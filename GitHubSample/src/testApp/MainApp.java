b8e03116fc267c83f46d9a89469a8a9f97b2272f
Bruce <Bruce03080308@gmail.com>
2023-01-07 11:46:37 +0800

    3 version (teammember)

--- GitHubSample/src/testApp/MainApp.java
+++ GitHubSample/src/testApp/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2 version");
+		// 組員撰寫的程式碼
+		System.out.println("3 version 組員");
 	}
 
 }
